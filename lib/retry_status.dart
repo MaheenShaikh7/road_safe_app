20e1a431724dbd498b08e31508acac55bea71105
Lucky Vishwakarma <110558983+LuckyVishwa1104@users.noreply.github.com>
2024-03-18 01:24:23 +0530

    added the retry page for invalid verifications

---
+++ lib/retry_status.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:road_safe_app/dashboard.dart';
+import 'package:road_safe_app/utils/app_drawer.dart';
+
+class retry extends StatefulWidget {
+  const retry({super.key});
+
+  @override
+  State<retry> createState() => _retryState();
+}
+
+class _retryState extends State<retry> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Complaint Status"), backgroundColor: Colors.amber),
+      drawer: const AppDrawer(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "There is issue in raising your complaint please, Retry it!",
+                style: TextStyle(fontSize: 25, ),
+              ),
+              SizedBox(height: 20,),
+              Center(
+                child: MaterialButton(
+                  color: Colors.amber,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text('Retry', style: TextStyle(fontSize: 20,),),
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => Dashboard()),
+                );
+
+                    // print('raising complaint');
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
