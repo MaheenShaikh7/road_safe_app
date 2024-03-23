f3090ac80e75f6c4e7a9c6ed81bdd08d689cefe5
Lucky Vishwakarma <110558983+LuckyVishwa1104@users.noreply.github.com>
2024-03-24 03:31:25 +0530

    added router for complaint backend

--- lgp_backend/app.js
+++ lgp_backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const body_parser = require('body-parser');
-const userRouter = require('./routers/user.router')
+const userRouter = require('./routers/user.router');
+const ComplaintRouter = require('./routers/complaint.router');
 
 const app = express();
 
 app.use(body_parser.json());
 
 app.use('/',userRouter);
+app.use('/',ComplaintRouter);
 
 module.exports = app;
