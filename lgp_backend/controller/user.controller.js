dd47b2d0905e09056f21952f42e95aecd1171f47
Lucky Vishwakarma <110558983+LuckyVishwa1104@users.noreply.github.com>
2024-03-24 19:50:26 +0530

    added response for non existing users

--- lgp_backend/controller/user.controller.js
+++ lgp_backend/controller/user.controller.js
@@ -16,15 +16,21 @@ exports.register = async(req,res,next) => {
 exports.login = async(req,res,next) => {
     try{
         const {email,password} = req.body;
+        // console.log("1st step")
         const user =await UserService.checkuser(email);
+        // console.log("2st step",user)
 
         if (!user){
-            throw new Error('User does not exist');
+            // throw new Error('User does not exist');
+            res.json({msg:"user does not exist"})
+            return;
         }
 
         const isMatch =await user.comparePassword(password);
         if (isMatch == false){
-            throw new Error('Password is invalid.'); 
+            res.json({msg:"password is invalid"})
+            return;
+            // throw new Error('Password is invalid.'); 
         }
 
         let tokenData = {_id: user._id,email: user.email};  
